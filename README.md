d95e18a6cd9241c60a92a06c4976077a15b3bd8e
Nithinkumar3000 <139805636+Nithinkumar3000@users.noreply.github.com>
2024-08-02 16:04:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# temprepo
+repository created for training. This will be deleted after training
+
+please delete this repository on August 3rd
---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+   
+
+// this method is to add numbers	
+	public int addNumbers(int a, int b){
+		return a + b;
+	}
+	
+	public int subtract (int a, int b){
+		
+		return a - b;
+	}
+}
